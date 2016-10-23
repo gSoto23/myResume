c7bf52a681851e7c98cc7e7f4f766af1eba9e346
Gerardo Soto <eddyngerardo@gmail.com>
2016-10-23 00:53:18 -0600

    Update adding files relate to my Resume

---
+++ js/hometest.js
@@ -0,0 +1,35 @@
+function init () {
+	$("#Box1").click(function(){
+		$("#Box1").css("background-color","pink");
+	});
+
+	$(".box-2").click(function(){
+		$(this).toggleClass("togAct");
+		$("h2").text("What's up bitch");
+	});
+
+	var x = document.getElementsByClassName("demo");
+	for (var i = 0; i < x.length; i++) {
+		x[i].innerHTML = " Test about how add text by js..";
+	}
+
+	/* Sticky Header*/
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 1){  
+			$('header').addClass("sticky");
+		}
+		else{
+			$('header').removeClass("sticky");
+		}
+	});
+
+	/* End of Sticky Header*/
+
+
+}
+
+$(document).ready(function(){
+	init();
+});
+
